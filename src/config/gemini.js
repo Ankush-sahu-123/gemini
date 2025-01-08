01a2356158af4675478fd8f161ea26f79beef876
Ankush-sahu-123 <ankushsahu1414@gmail.com>
2025-01-09 02:30:52 +0530

    Initial commit

---
+++ src/config/gemini.js
@@ -0,0 +1,43 @@
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ *
+ * See the getting started guide for more information
+ * https://ai.google.dev/gemini-api/docs/get-started/node
+ */
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from"@google/generative-ai";
+
+  const apiKey = "AIzaSyCcUhRtGMk8TDfCtaNxrqvxnPfjTwj3bxc";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash-exp",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function runChat(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+    return result.response.text();
+  }
+  
+ export default runChat;
